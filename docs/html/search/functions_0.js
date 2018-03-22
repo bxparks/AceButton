d5e06cb44832706e9099d271a3e7206d86c6d9a8
Brian Park <brian@xparks.net>
2018-03-21 18:35:42 -0700

    Update doxygen docs for publication using GitHub Pages

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['acebutton',['AceButton',['../classace__button_1_1AceButton.html#a8226a57ba83698e0023b8062d651396f',1,'ace_button::AceButton']]],
+  ['acebutton',['AceButton',['../classace__button_1_1AceButton.html#a3c480636223edc899a79c821c32c6982',1,'ace_button::AceButton']]],
   ['addevent',['addEvent',['../classace__button_1_1testing_1_1EventTracker.html#a39a77bf9a36deb800fcebeaa88c106ce',1,'ace_button::testing::EventTracker']]]
 ];
