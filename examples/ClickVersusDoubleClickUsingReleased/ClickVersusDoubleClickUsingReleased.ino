238844e9547485c5919edc84accb23fa8d784231
Brian Park <brian@xparks.net>
2018-03-25 16:58:23 -0700

    Implement kFeatureSuppressClickBeforeDoubleClick feature and add unit test for it

--- examples/ClickVersusDoubleClickUsingReleased/ClickVersusDoubleClickUsingReleased.ino
+++ examples/ClickVersusDoubleClickUsingReleased/ClickVersusDoubleClickUsingReleased.ino
@@ -1,38 +1,42 @@
 /*
- * A demo of a program which distinguishes a "Clicked" from a "DoubleClicked". A
- * Clicked turns on the LED. A DoubleClick turns off the LED.
+ * A demo that distinguishes a "Clicked" from a "DoubleClicked" by using a
+ * Released event intead. "Clicked" (actually Released) turns on the LED. A
+ * DoubleClick turns off the LED.
  *
- * The current AceButton code cannot actually separate a Clicked from a
- * DoubleClicked because the Clicked event will always trigger if a
- * DoubleClicked occurs. We cannot suppress the first Clicked because it's
- * already happened in the past by the time the DoubleClicked occurs, and the
- * first Clicked cannot predict the future.
+ * Normally, AceButton cannot actually separate a Clicked from a DoubleClicked
+ * because the Clicked event will always trigger if a DoubleClicked occurs. We
+ * cannot suppress the first Clicked because it's already happened in the past
+ * by the time the DoubleClicked occurs, and the first Clicked cannot predict
+ * the future.
  *
  * The only way to suppress the Clicked "after" a DoubleClicked is to delay the
  * sending of the Clicked event until we wait getDoubleClickDelay() time after
  * the Clicked, at which time we can tell if a DoubleClicked as occurred or not.
  * But this algorithm means that every Clicked event would be delayed by
  * (kClickDelay + kDoubleClickDelay + 2 * kDebounceDelay) which is 700 ms using
- * the default values. AceButton does not currently implement this feature
- * because the delay seemed too long.
+ * the default values.
  *
- * The workaround is to use a Released event instead of a Clicked to trigger the
+ * This workaround uses a Released event instead of a Clicked to trigger the
  * first action, then use the DoubleClick for the second action, while
  * suppressing a Released after a DoubleClicked. The drawback to this workaround
  * is that it is no longer possible to distinguish between a normal
- * Pressed/Released input from a Clicked input. Often this is actually the what
- * we want, because users are not always physically able to perform the two
+ * Pressed/Released input from a Clicked input. But often this is actually the
+ * what we want, because users are not always physically able to perform the two
  * types of inputs accurately. In other words, it's too easy for a user to
  * mistakenly trigger Clicked when they meant to just trigger a
  * Pressed/Released, or the reverse. By simply ignoring the Clicked, we
  * sometimes get a more reliable user interface.
+ *
+ * See Also:
+ *    "examples/ClickPostponedAfterDoubleClick/" program which uses
+ *    kFeatureSuppressClickBeforeDoubleClick flag.
  */
 
 #include <AceButton.h>
 using namespace ace_button;
 
 // The pin number attached to the button.
-const int BUTTON_PIN = 6;
+const int BUTTON_PIN = 2;
 
 // LED states. Some microcontrollers wire their built-in LED the reverse.
 const int LED_ON = HIGH;
@@ -55,6 +59,7 @@ void setup() {
   // automatically implies kEventClicked.
   ButtonConfig* buttonConfig = button.getButtonConfig();
   buttonConfig->setEventHandler(handleEvent);
+  buttonConfig->setFeature(ButtonConfig::kFeatureSuppressAfterClick);
   buttonConfig->setFeature(ButtonConfig::kFeatureDoubleClick);
   buttonConfig->setFeature(ButtonConfig::kFeatureSuppressAfterDoubleClick);
 }
