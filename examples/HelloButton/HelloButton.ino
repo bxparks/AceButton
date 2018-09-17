753a33650922eab9c78090f0be6a89487d54006a
Brian Park <brian@xparks.net>
2018-09-16 23:36:34 -0700

    HelloButton: update to match latest AUniter INI file format

--- examples/HelloButton/HelloButton.ino
+++ examples/HelloButton/HelloButton.ino
@@ -8,28 +8,41 @@
 #include <AceButton.h>
 using namespace ace_button;
 
-#if defined(AUNITER_NANO)
-const int BUTTON_PIN = AUNITER_RIGHT_BUTTON;
-#else
-const int BUTTON_PIN = 2; // change this to the button pin
+#if !defined(AUNITER)
+  #define AUNITER_MICRO
+  #warning Assuming AUNITER_MICRO
 #endif
 
-#ifdef ESP32
-  // Different ESP32 boards use different pins
+#if defined(AUNITER_NANO)
+  const int BUTTON_PIN = AUNITER_RIGHT_BUTTON;
+  const int LED_PIN = LED_BUILTIN;
+  const int LED_ON = HIGH;
+  const int LED_OFF = LOW;
+#elif defined(AUNITER_MICRO)
+  const int BUTTON_PIN = 3;
+  const int LED_PIN = LED_BUILTIN_RX;
+  const int LED_ON = LOW;
+  const int LED_OFF = HIGH;
+#elif defined(AUNITER_ESP32)
+  // Different ESP32 boards use different pins, so just pick one for now
   const int LED_PIN = 2;
 #else
+  const int BUTTON_PIN = 2; // change this to the button pin
   const int LED_PIN = LED_BUILTIN;
 #endif
-
-// LED states. Some microcontrollers wire their built-in LED the reverse.
-const int LED_ON = HIGH;
-const int LED_OFF = LOW;
 
 AceButton button(BUTTON_PIN);
 
 void handleEvent(AceButton*, uint8_t, uint8_t);
 
 void setup() {
+  delay(2000);
+
+#if defined(ARDUINO_AVR_LEONARDO)
+  RXLED0; // LED off
+  TXLED0; // LED off
+#endif
+
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   button.setEventHandler(handleEvent);
