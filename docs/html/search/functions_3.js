56cacb213be816fcad200d8d495e2d6ad1f65c1b
Brian Park <brian@xparks.net>
2019-07-06 09:26:35 -0700

    docs: Update doxygen docs to 1.3.4

--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['getbuttonconfig',['getButtonConfig',['../classace__button_1_1AceButton.html#a5e4ea4aca852a5e0b1d426bccb131cfb',1,'ace_button::AceButton']]],
+  ['getbuttonconfig',['getButtonConfig',['../classace__button_1_1AceButton.html#af5e39bdfbc0e6bbd9c9191029d29685e',1,'ace_button::AceButton']]],
   ['getclickdelay',['getClickDelay',['../classace__button_1_1ButtonConfig.html#a19db203ce87dc2cafb2cab6ba124387a',1,'ace_button::ButtonConfig']]],
   ['getclock',['getClock',['../classace__button_1_1ButtonConfig.html#a440271e22d5e7e161cdcb3b73f092111',1,'ace_button::ButtonConfig']]],
   ['getclockmicros',['getClockMicros',['../classace__button_1_1ButtonConfig.html#a8a74d1a1c082bfb0e4ddf55e67d3ac2d',1,'ace_button::ButtonConfig']]],
@@ -9,14 +9,14 @@ var searchData=
   ['getdebouncedelay',['getDebounceDelay',['../classace__button_1_1ButtonConfig.html#ad7be58d0fe0b242f78f15319d201a565',1,'ace_button::ButtonConfig']]],
   ['getdefaultreleasedstate',['getDefaultReleasedState',['../classace__button_1_1AceButton.html#afa8013bb98f5fbd79654aebca613ba71',1,'ace_button::AceButton']]],
   ['getdoubleclickdelay',['getDoubleClickDelay',['../classace__button_1_1ButtonConfig.html#a5ba500367e03c36f39225a5858e110e6',1,'ace_button::ButtonConfig']]],
-  ['geteventhandler',['getEventHandler',['../classace__button_1_1ButtonConfig.html#a8fc32804ba102f61ba97bcc5f4fe8448',1,'ace_button::ButtonConfig']]],
+  ['geteventhandler',['getEventHandler',['../classace__button_1_1ButtonConfig.html#af54641e72e5624be0eb57a012c5372bd',1,'ace_button::ButtonConfig']]],
   ['getexpdecayavg',['getExpDecayAvg',['../classace__button_1_1TimingStats.html#a04c9dbea755eafeed3bb7872d7fb0d24',1,'ace_button::TimingStats']]],
-  ['getid',['getId',['../classace__button_1_1AceButton.html#a4a693b836e3fed29d4e7203a5a97910b',1,'ace_button::AceButton']]],
-  ['getlastbuttonstate',['getLastButtonState',['../classace__button_1_1AceButton.html#a3c49e9edf8cbb5e3800c5f4506d0beca',1,'ace_button::AceButton']]],
+  ['getid',['getId',['../classace__button_1_1AceButton.html#ae81fb47e01b375ce1d79de3c8496ff21',1,'ace_button::AceButton']]],
+  ['getlastbuttonstate',['getLastButtonState',['../classace__button_1_1AceButton.html#acbb18322e7560cd526061918c50d462f',1,'ace_button::AceButton']]],
   ['getlongpressdelay',['getLongPressDelay',['../classace__button_1_1ButtonConfig.html#a46d4213a827a94bf82ba99a9c2e59531',1,'ace_button::ButtonConfig']]],
-  ['getpin',['getPin',['../classace__button_1_1AceButton.html#ac6b12ca62c7c055325bfebd036579ed6',1,'ace_button::AceButton']]],
+  ['getpin',['getPin',['../classace__button_1_1AceButton.html#a64376f91e9053abb2a2040dd3c41daeb',1,'ace_button::AceButton']]],
   ['getrepeatpressdelay',['getRepeatPressDelay',['../classace__button_1_1ButtonConfig.html#ad85c75d2a2dd200552a181fb154d5fd2',1,'ace_button::ButtonConfig']]],
   ['getrepeatpressinterval',['getRepeatPressInterval',['../classace__button_1_1ButtonConfig.html#aec4a97a1a1306b9ff12f1cdf44515c43',1,'ace_button::ButtonConfig']]],
-  ['getsystembuttonconfig',['getSystemButtonConfig',['../classace__button_1_1ButtonConfig.html#ab780f081ef691b48deb5182d206a4b9d',1,'ace_button::ButtonConfig']]],
+  ['getsystembuttonconfig',['getSystemButtonConfig',['../classace__button_1_1ButtonConfig.html#a585353712d2c76d91c10e67102cb614d',1,'ace_button::ButtonConfig']]],
   ['gettimingstats',['getTimingStats',['../classace__button_1_1ButtonConfig.html#aff7101f2fe0f0c3bba67924b01e9d493',1,'ace_button::ButtonConfig']]]
 ];
