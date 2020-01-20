fb398c4e5ab5182f642700e276f85730d312e035
Brian Park <brian@xparks.net>
2020-01-20 12:47:40 -0800

    docs: Regenerate doxygen docs for 1.4.1

--- docs/html/search/classes_2.js
+++ docs/html/search/classes_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['encoded4to2buttonconfig',['Encoded4To2ButtonConfig',['../classace__button_1_1Encoded4To2ButtonConfig.html',1,'ace_button']]],
-  ['encoded8to3buttonconfig',['Encoded8To3ButtonConfig',['../classace__button_1_1Encoded8To3ButtonConfig.html',1,'ace_button']]]
+  ['encoded8to3buttonconfig',['Encoded8To3ButtonConfig',['../classace__button_1_1Encoded8To3ButtonConfig.html',1,'ace_button']]],
+  ['encodedbuttonconfig',['EncodedButtonConfig',['../classace__button_1_1EncodedButtonConfig.html',1,'ace_button']]]
 ];
