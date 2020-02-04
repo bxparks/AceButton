7cbcee9185aa6c2007d0a71bcb0fac9ed6bf790f
Brian Park <brian@xparks.net>
2020-02-03 22:01:02 -0800

    docs: Generate doxygen docs for 1.4.2

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['encoded4to2buttonconfig',['Encoded4To2ButtonConfig',['../classace__button_1_1Encoded4To2ButtonConfig.html',1,'ace_button::Encoded4To2ButtonConfig'],['../classace__button_1_1Encoded4To2ButtonConfig.html#ad04fcc5c8bf65b83a8a9bc9b47a570c3',1,'ace_button::Encoded4To2ButtonConfig::Encoded4To2ButtonConfig()']]],
   ['encoded8to3buttonconfig',['Encoded8To3ButtonConfig',['../classace__button_1_1Encoded8To3ButtonConfig.html',1,'ace_button::Encoded8To3ButtonConfig'],['../classace__button_1_1Encoded8To3ButtonConfig.html#a8415b8569aff2a5a5aea3fba8e9dfc4c',1,'ace_button::Encoded8To3ButtonConfig::Encoded8To3ButtonConfig()']]],
-  ['encodedbuttonconfig',['EncodedButtonConfig',['../classace__button_1_1EncodedButtonConfig.html',1,'ace_button::EncodedButtonConfig'],['../classace__button_1_1EncodedButtonConfig.html#a5a0cebd1a867ef53619afa8526341e3f',1,'ace_button::EncodedButtonConfig::EncodedButtonConfig()']]],
+  ['encodedbuttonconfig',['EncodedButtonConfig',['../classace__button_1_1EncodedButtonConfig.html',1,'ace_button::EncodedButtonConfig'],['../classace__button_1_1EncodedButtonConfig.html#a5538117156240cf8ee188a974db43586',1,'ace_button::EncodedButtonConfig::EncodedButtonConfig()']]],
   ['eventhandler',['EventHandler',['../classace__button_1_1ButtonConfig.html#a6d9db3c7b221b474c3cfd8fca5f4ba1e',1,'ace_button::ButtonConfig']]]
 ];
