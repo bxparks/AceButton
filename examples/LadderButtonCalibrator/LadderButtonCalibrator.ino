f282043b6b0b9ce38dc61e68d747969ad992cd5e
Brian Park <brian@xparks.net>
2021-05-07 09:57:42 -0700

    examples/LadderButtonCalibrator: Add option to print to a TM1637 LED Module instead of Serial, for boards w/o easy Serial monitor, e.g. ATtiny85

--- examples/LadderButtonCalibrator/LadderButtonCalibrator.ino
+++ examples/LadderButtonCalibrator/LadderButtonCalibrator.ino
@@ -8,6 +8,50 @@
 #include <stdint.h>
 #include <Arduino.h>
 
+//-----------------------------------------------------------------------------
+
+// Choose how to output the results. Normally, we would send to the Serial
+// monitor. But some environments (e.g. ATtiny85) do not normally provide a
+// USB-to-Serial monitor. If we can hook up a seven-segment LED module, we can
+// use AceSegment (https://github.com/bxparks/AceSegment) to print to the LED
+// module.
+#define OUTPUT_TYPE_SERIAL 0
+#define OUTPUT_TYPE_TM1637 1
+#define OUTPUT_TYPE OUTPUT_TYPE_TM1637
+
+#if OUTPUT_TYPE == OUTPUT_TYPE_TM1637
+  #include <AceSegment.h>
+  using ace_segment::SoftWireInterface;
+  using ace_segment::Tm1637Module;
+  using ace_segment::LedDisplay;
+  using ace_segment::NumberWriter;
+
+  // These pins are for my ATtiny85 board.
+  const uint8_t CLK_PIN = 3;
+  const uint8_t DIO_PIN = 1;
+  const uint16_t BIT_DELAY = 100;
+  const uint8_t NUM_DIGITS = 4;
+
+  using WireInterface = SoftWireInterface;
+  WireInterface wireInterface(CLK_PIN, DIO_PIN, BIT_DELAY);
+  Tm1637Module<WireInterface, NUM_DIGITS> tm1637Module(wireInterface);
+  LedDisplay ledDisplay(tm1637Module);
+  NumberWriter numberWriter (ledDisplay);
+
+  void flushLed() {
+    static uint16_t prevMillis;
+
+    uint16_t nowMillis = millis();
+    if (nowMillis - prevMillis >= 100) {
+      prevMillis = nowMillis;
+      tm1637Module.flush();
+    }
+  }
+
+#endif
+
+//-----------------------------------------------------------------------------
+
 // Define the actual ADC pin.
 static const uint8_t BUTTON_PIN = A0;
 
@@ -23,7 +67,13 @@ void calibrateAnalogRead() {
     prevMillis = nowMillis;
 
     uint16_t val = analogRead(BUTTON_PIN);
+
+  #if OUTPUT_TYPE == OUTPUT_TYPE_SERIAL
     Serial.println(val);
+  #elif OUTPUT_TYPE == OUTPUT_TYPE_TM1637
+    numberWriter.writeUnsignedDecimalAt(0, val, 4 /*boxSize*/);
+  #endif
+
   }
 }
 
@@ -32,17 +82,31 @@ void calibrateAnalogRead() {
 void setup() {
   delay(1000); // some microcontrollers reboot twice
 
+#if OUTPUT_TYPE == OUTPUT_TYPE_SERIAL
   Serial.begin(115200);
   while (! Serial); // Wait until Serial is ready - Leonardo/Micro
   Serial.println(F("setup(): begin"));
+
+#elif OUTPUT_TYPE == OUTPUT_TYPE_TM1637
+  wireInterface.begin();
+  tm1637Module.begin();
+  ledDisplay.setBrightness(3);
+#endif
 
   // Don't use internal pull-up resistor because it will change the effective
   // resistance of the resistor ladder.
   pinMode(BUTTON_PIN, INPUT);
 
+#if OUTPUT_TYPE == OUTPUT_TYPE_SERIAL
   Serial.println(F("setup(): ready"));
+#endif
 }
 
 void loop() {
   calibrateAnalogRead();
+
+#if OUTPUT_TYPE == OUTPUT_TYPE_TM1637
+  flushLed();
+#endif
+
 }
